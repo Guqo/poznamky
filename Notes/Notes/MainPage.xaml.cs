860a60e3da60c10bb0a9446a0c9429c2e052f16c
Guqo <ryto04@seznam.cz>
2023-05-17 21:08:44 +0200

    01

---
+++ Notes/Notes/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Btn_Clicked(object sender, EventArgs e)
+        {
+            string datum = date.Text;
+            string poznamka = poznamkaEntry.Text;
+        await DisplayAlert($"Tvá poznámka ze dne {datum}:", $"{poznamka}", "OK");
+        }
+    }
+}
